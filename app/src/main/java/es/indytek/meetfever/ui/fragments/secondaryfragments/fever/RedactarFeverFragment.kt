80b7f44873ca150bfad1d0925db46346724f7b88
jujo0922 <juliolandazuridiaz@outlook.es>
2022-05-19 15:41:22 +0200

    ni idea que me pide

--- app/src/main/java/es/indytek/meetfever/ui/fragments/secondaryfragments/fever/RedactarFeverFragment.kt
+++ app/src/main/java/es/indytek/meetfever/ui/fragments/secondaryfragments/fever/RedactarFeverFragment.kt
@@ -1,8 +1,10 @@
 package es.indytek.meetfever.ui.fragments.secondaryfragments.fever
 
+import android.annotation.SuppressLint
 import android.graphics.BitmapFactory
 import android.graphics.Color
 import android.graphics.PorterDuff
+import android.icu.number.Scale.none
 import android.os.Bundle
 import android.text.Spannable
 import android.text.style.ForegroundColorSpan
@@ -11,7 +13,10 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AnimationUtils
 import android.widget.ImageView
+import android.widget.Toast
+import androidx.core.view.ViewCompat
 import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import es.indytek.meetfever.R
@@ -22,9 +27,15 @@ import es.indytek.meetfever.databinding.FragmentRedactarFeverBinding
 import es.indytek.meetfever.models.emoticono.Emoticono
 import es.indytek.meetfever.models.emoticono.EmoticonoWrapper
 import es.indytek.meetfever.models.empresa.Empresa
+import es.indytek.meetfever.models.experiencia.Experiencia
+import es.indytek.meetfever.models.opinion.Opinion
 import es.indytek.meetfever.models.usuario.Usuario
+import es.indytek.meetfever.utils.Animations
+import es.indytek.meetfever.utils.Animations.agrandarViewSuavemente
+import es.indytek.meetfever.utils.Animations.reducirViewSuavemente
 import es.indytek.meetfever.utils.Utils
 import kotlinx.coroutines.*
+import java.security.KeyStore
 
 private const val ARG_PARAM1 = "currentUsuario"
 
@@ -33,7 +44,9 @@ class RedactarFeverFragment : Fragment() {
     private lateinit var binding: FragmentRedactarFeverBinding
     private lateinit var currentUsuario: Usuario
     private lateinit var emoticonos: ArrayList<Emoticono>
-    private lateinit var imageViews: ArrayList<ImageView>
+    private lateinit var imageViews: LinkedHashMap<ImageView, Boolean>
+    private var idEmpresa: Int = 0
+    private var idExperiencia: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,7 +55,7 @@ class RedactarFeverFragment : Fragment() {
         }
 
         emoticonos = ArrayList()
-        imageViews = ArrayList()
+        imageViews = linkedMapOf()
     }
 
     override fun onCreateView(
@@ -52,19 +65,31 @@ class RedactarFeverFragment : Fragment() {
 
         binding = FragmentRedactarFeverBinding.inflate(inflater, container, false)
 
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
         // pinto to.do lo necesario
         pintar()
 
         // arranco el contador de caracteres y lo limito a 250
         procesadorDeTextoYCaracteres()
 
-        return binding.root
+        loadListeners()
+    }
+
+    private fun loadListeners(){
+        binding.botonSendFever.setOnClickListener { sendFeverToServer() }
     }
 
     // cuento que el usuario no supere los 250 caracteres
     private fun procesadorDeTextoYCaracteres() {
 
         var empresaEncontrada = false
+        var experienciaEncontrada = false
 
         binding.opinionContainer.addTextChangedListener { text ->
 
@@ -107,6 +132,8 @@ class RedactarFeverFragment : Fragment() {
 
                                     // pongo la foto en la preview
                                     val foto = empresaDescargada.fotoPerfil
+                                    this@RedactarFeverFragment.idEmpresa = empresaDescargada.id
+
                                     foto?.let {
                                         Utils.putBase64ImageIntoImageViewWithPlaceholder(binding.previewEnterprise, foto, requireContext(), R.drawable.ic_default_enterprise_black_and_white)
                                     } ?: run {
@@ -160,6 +187,37 @@ class RedactarFeverFragment : Fragment() {
                         binding.opinionContainer.setTextColor(requireContext().getColor(R.color.gris_textos))
                     }
 
+                }else if(text.contains("#")){
+                    // obtengo las palabras que empiezan por #
+                    val experiencias = text.toString().split(" ").filter {
+                        it.startsWith("#")
+                    }
+
+                    val experiencia = experiencias[0]
+
+                    val experienciaCortada = experiencia.substring(1, experiencia.length)
+
+                    Log.d(":::", "EMPRESA ENCONTRADA -> $experienciaEncontrada")
+                    Log.d(":::", "EMPRESA CORTADA -> $experienciaCortada")
+
+
+
+
+                }else{
+                    empresaEncontrada = false
+
+                    binding.opinionContainer.text?.setSpan(ForegroundColorSpan(requireContext().getColor(R.color.gris_textos)), 0, text.length, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+                    // oculto la foto etc
+                    binding.nombreEmpresa.visibility = View.GONE
+                    binding.descripcionEmpresa.visibility = View.GONE
+                    binding.previewEnterprise.visibility = View.GONE
+                    binding.degradado.visibility = View.GONE
+                    binding.previewEnterprise.setImageDrawable(null)
+
+                    // Vuelvo a mostrar lo de por defecto
+                    binding.noSeleccionadoPreview.visibility = View.VISIBLE
+                    binding.elLocalQueSeleccione.visibility = View.VISIBLE
                 }
 
             }
@@ -202,22 +260,16 @@ class RedactarFeverFragment : Fragment() {
 
                     val e = any as EmoticonoWrapper
 
-                    //Animations.ocultarVistaSuavemente(binding.animationView, 500)
+                    Animations.ocultarVistaSuavemente(binding.animationView, 500)
 
                     try {
-//                        Animations.pintarGridRecyclerViewSuavemente(
-//                            gridLayoutManager = GridLayoutManager(requireContext(), 11),
-//                            recyclerView = binding.listaDeIconos,
-//                            adapter = IconoRecyclerViewAdapter(emoticonos),
-//                            duration = 200
-//                        )
 
                         emoticonos.addAll(e)
 
                         createEmoticonoImageViews()
 
                     } catch (e: Exception) {
-                        Log.d(":::","¿Tienes un móvil o una tostadora? no le dió tiempo a cargar al context")
+                        e.printStackTrace()
                     }
 
 
@@ -229,6 +281,8 @@ class RedactarFeverFragment : Fragment() {
 
     }
 
+
+    @SuppressLint("UseCompatLoadingForDrawables")
     fun createEmoticonoImageViews(){
 
         val emoticonosViews = ArrayList<ImageView>()
@@ -240,15 +294,24 @@ class RedactarFeverFragment : Fragment() {
             val imageView = ImageView(requireContext())
             imageView.setImageBitmap(decodedByte)
 
-            imageViews.add(imageView)
+            imageViews[imageView] = false
+
             imageView.setOnClickListener {
+
+                imageView.startAnimation(AnimationUtils.loadAnimation(requireContext(), androidx.appcompat.R.anim.abc_popup_enter))
+
+                imageView.clearColorFilter()
+
                 imageViews.forEach {
-                    it.clearColorFilter()
+                    if(it.key != imageView){
+                        imageViews[it.key] = false
+
+                        it.key.setColorFilter(Color.GRAY, PorterDuff.Mode.SRC_ATOP)
+                    }
+
                 }
 
-                imageView.setColorFilter(requireContext().getColor(R.color.rosa_meet), PorterDuff.Mode.SRC_ATOP)
-
-                //TODO QUEDARME CON EL QUE SELECCIONE
+                imageViews[imageView] = true
             }
 
             emoticonosViews.add(imageView)
@@ -259,20 +322,39 @@ class RedactarFeverFragment : Fragment() {
 
         val layout = binding.lechuga
 
+        var animTime = 500L
         //le asigno una posición
         imageViews.forEach {
 
-            layout.addView(it)
+            layout.addView(it.key)
+            Animations.mostrarVistaSuavemente(it.key, animTime)
 
             // le doy una altura de 100
-            it.layoutParams.height = 80
-            it.layoutParams.width = 80
-//            it.layoutParams.
+            it.key.layoutParams.height = 140
+            it.key.layoutParams.width = 140
+            it.key.setPadding(20, 20, 20, 20)
 
+            animTime+= 100L
 
         }
+    }
 
+    private fun sendFeverToServer(){
 
+        val imageView = imageViews.filter { (_,v) -> !v }.toList()[0]
+        val emoticono = emoticonos[imageViews.toList().indexOf(imageView)]
+
+        val opinion =  Opinion(
+            descripcion = binding.opinionContainer.text.toString(),
+            eMOTICONO = emoticono,
+            autor = currentUsuario,
+            idEmpresa = idEmpresa,
+            idExperiencia = idExperiencia,
+            titulo = ""
+
+        )
+
+        Log.d(":::", opinion.toString())
 
 
     }
